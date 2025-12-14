0f99dcc86f6541e996f183e1bd9c537641ddc017
최원혁 <asoe72@gmail.com>
2025-12-14 12:02:37 +0900

    시험용 코드 remark

--- vector_db.js
+++ vector_db.js
@@ -54,8 +54,8 @@ export const upload = async (openai, index, pathnameBookshelves, options) => {
   const metadatas = await loadMetadatasFromBookshelves(pathnameBookshelves);
 
   // 개수 제한 (시험용)
-  const n_max = 20;
-  metadatas.length = n_max;
+  //const n_max = 20;
+  //metadatas.length = n_max;
 
   // vector DB에 upload
   if(options?.skipUploadToDb==undefined) {
